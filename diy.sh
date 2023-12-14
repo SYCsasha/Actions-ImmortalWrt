7df1ff4f119ef23ba26039b3dc7955ea8af231b6
SYCsasha <124485434+SYCsasha@users.noreply.github.com>
2023-12-14 14:20:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,9 +3,9 @@
 # git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # git clone https://github.com/kongfl888/luci-app-timedreboot package/timedreboot
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone https://github.com/gdy666/lucky package/lucky
-git clone https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
-git clone https://github.com/sbwml/luci-app-alist package/alist
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone https://github.com/gdy666/lucky package/lucky
+# git clone https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
+# git clone https://github.com/sbwml/luci-app-alist package/alist
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
